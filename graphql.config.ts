ed09e1749b4eec0a571db4db157c0d7370286a49
Jodee-Arat <obgon02@mail.ru>
2026-05-15 14:59:23 +0700

    Use remote GraphQL schema for codegen

--- graphql.config.ts
+++ graphql.config.ts
@@ -1,8 +1,12 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 import "dotenv/config";
 
+const schema =
+  process.env.NEXT_PUBLIC_SERVER_URL ??
+  "../../backend/src/core/graphql/schema.gql";
+
 const config: CodegenConfig = {
-  schema: "../../backend/src/core/graphql/schema.gql",
+  schema,
   documents: ["./src/shared/graphql/**/*.graphql"],
   generates: {
     "./src/shared/graphql/generated/output.ts": {
